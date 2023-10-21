d1eefae39e3759b151f7c4b7de9731a7be33a91b
raspiAB <akbatra567@gmail.com>
2023-10-22 02:12:57 +0530

    moving to express instead of vite

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req:express.Request, res:express.Response) => {
+
+res.send("hello world");
+})
+
+app.listen(3000, '192.168.29.249', () => {
+console.log("server running at localhost:3000");
+})
