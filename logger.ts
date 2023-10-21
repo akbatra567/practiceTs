446a6aa11d9451fed2572049aa59207b0200718e
raspiAB <akbatra567@gmail.com>
2023-10-22 03:37:56 +0530

    WIP winston logger

---
+++ logger.ts
@@ -0,0 +1,12 @@
+const winston = require("winston");
+const logger = winston.creatLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+export default logger;
--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import logger from "./logger";
+
 const app = express();
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("hello world");
 });
 
-app.listen(3000, "192.168.29.249", () => {
-  console.log("server running at localhost:3000");
+app.listen(3000, () => {
+  logger.info("server running at localhost:3000");
 });
